df6c9c07bdcba81b0510b982491de807cfc91acd
MeenakshiM25 <mail.m.meenakshi@gmail.com>
2018-03-01 19:22:57 +0530

    Update init.js

--- EditableListView/src/main/resources/com/tavant/wicket/js/init.js
+++ EditableListView/src/main/resources/com/tavant/wicket/js/init.js
@@ -8,7 +8,7 @@ var testinghosts = ["xxx.com"
 
 //stage Akamai, prod CMS
 if((dtmLoc.indexOf(".xxx.") >= 0) && (testinghosts.indexOf(dtmLoc) < 0)){
-  document.write('<script src="//www.53.com/dtm/e251f8161031ba53e6aefc36918d7e8f02c5e526/satelliteLib-0bf6a6472452184702edffb1fed34b2831ac0b7a.js"></script>');
+  document.write('<script src="//www.xxx.com/dtm/e251f8161031ba53e6aefc36918d7e8f02c5e526/satelliteLib-0bf6a6472452184702edffb1fed34b2831ac0b7a.js"></script>');
   //console.log('PROD DTM');
 } else {
   document.write('<script src="//assets.adobedtm.com/e251f8161031ba53e6aefc36918d7e8f02c5e526/satelliteLib-0bf6a6472452184702edffb1fed34b2831ac0b7a-staging.js"></script>');
